44ed2c9ca461a702cc7b67c35da240553660b51a
Michael Simoes <mikes88@live.ca>
2023-02-02 18:14:11 -0800

    Refactored readMail.js file

--- readMail.js
+++ readMail.js
@@ -19,43 +19,44 @@ function openTheInbox(cb) {
 function readNewestEmail(emailClient) { 
     imap.on('ready', () => {
         openTheInbox((err, box) => {
-        if (err) {throw new Error('Something has gone wrong with the openInbox function: ', err)}
-        const latestMessage = imap.seq.fetch(box.messages.total + ':*', { 
-            bodies: 
-            ['HEADER.FIELDS (FROM SUBJECT)',
-            'TEXT'],  
-        }) 
-        latestMessage.on('message', (msg) => {   
-            let count = 0
-            let body = ''
-            let header = ''
-            msg.on('body', (stream) => {          ///RUNS TWICE (for email header and body)
-            count++
-            stream.on('data', (chunk) => {           ///RUNS AS MANY TIMES AS IS NECESSARY for each data stream (header and body)       
-                if (emailClient == 'gmail') {
-                    if (count == 1) { 
-                        body += chunk.toString('utf8')                 
-                    }
+            if (err) {throw new Error('Something has gone wrong with the openInbox function: ', err)}
+            const latestMessage = imap.seq.fetch(box.messages.total + ':*', 
+                { 
+                    bodies: 
+                    ['HEADER.FIELDS (FROM SUBJECT)',
+                    'TEXT'],  
+                }) 
+            latestMessage.on('message', (msg) => {   
+                let count = 0
+                let body = ''
+                let header = ''
+                msg.on('body', (stream) => {            ///RUNS TWICE (for email header and body)
+                count++
+                stream.on('data', (chunk) => {           ///RUNS AS MANY TIMES AS IS NECESSARY for each data stream (header and body)       
+                    if (emailClient == 'gmail') {
+                        if (count == 1) { 
+                            body += chunk.toString('utf8')                 
+                        }
+                        else {
+                            header += chunk.toString('utf8')                  
+                        }               
+                    }                                                              
                     else {
-                        header += chunk.toString('utf8')                  
-                    }               
-                }                                                              
-                if (emailClient != 'gmail') {
-                    if (count == 1) { 
-                        header += chunk.toString('utf8')                  
-                    }
-                    else {
-                        body += chunk.toString('utf8')                  
-                    }               
-                }    
+                        if (count == 1) { 
+                            header += chunk.toString('utf8')                  
+                        }
+                        else {
+                            body += chunk.toString('utf8')                  
+                        }               
+                    }    
+                }) 
+                })
+                msg.once('end', () => {
+                    parseEmail(body, header)
+                    latestMessage.once('end', () => {imap.end()})
+                })    
             }) 
             })
-            msg.once('end', () => {
-                parseEmail(body, header)
-                latestMessage.once('end', () => {imap.end()})
-            })    
-        }) 
-        })
     })
 }
 
@@ -118,48 +119,62 @@ function removeExtraData(body) {
     return body
 }
 
-async function parseEmail(body, header) {
-    plainBody = await convertHtml(body)
-    cleanBody = await cleanString(plainBody)
-    cleanBody = removeExtraData(cleanBody)
+function removeReplyThread(body) {
+     //Remove reply threads that are indented
+     let cleanBody = body
+     splitArray = cleanBody.indexOf('> On ')
+     if (splitArray != -1) {
+         cleanBody = cleanBody.split('> On ').slice(0, 1).join('')
+     }
+     
+     //Remove reply threads that are not indented but have a "from, sent, to, subject" header attached
+     splitText = cleanBody.split('\n')
+     for (let i = 0; i < splitText.length -4; i++) {
+         if (splitText[i].indexOf('From: ') == 0 &&
+             splitText[i + 1].indexOf('Sent: ') == 0 &&
+             splitText[i + 2].indexOf('To: ') == 0 &&
+             splitText[i + 3].indexOf('Subject: ') == 0) {
+             splitText = splitText.slice(0, i)
+             cleanBody = splitText.join('\n')
+             break
+         }
+     }
+     //Remove reply threads characterized by the ">" character on every line, and "<email address> wrote:" on the line before
+     for (let i = 0; i < splitText.length -3; i++) {
+         if (splitText[i].indexOf('> wrote:') > -1 &&
+             splitText[i + 3].indexOf('>') == 0 ||
+             splitText[i + 2].indexOf('>') == 0 &&
+             splitText[i + 4].indexOf('>') == 0 &&
+             splitText[i + 5].indexOf('Subject: ') == 0) {
+             splitText = splitText.slice(0, i)
+             cleanBody = splitText.join('\n')
+             break
+         }
+     }
+    return cleanBody
+}
 
-    //Remove reply threads that are indented
-    splitArray = cleanBody.indexOf('> On ')
-    if (splitArray != -1) {
-        cleanBody = cleanBody.split('> On ').slice(0, 1).join('')
-    }
-    //Here we have splitText split based on new line character
-    //Remove reply threads that are not indented but have a "from, sent, to, subject" header attached
-    splitText = cleanBody.split('\n')
-    for (let i = 0; i < splitText.length -4; i++) {
-        if (splitText[i].indexOf('From: ') == 0 &&
-            splitText[i + 1].indexOf('Sent: ') == 0 &&
-            splitText[i + 2].indexOf('To: ') == 0 &&
-            splitText[i + 3].indexOf('Subject: ') == 0) {
-            splitText = splitText.slice(0, i)
-            cleanBody = splitText.join('\n')
-            break
-        }
-    }
-    //Remove reply threads characterized by the ">" character on every line, and "<email address> wrote:" on the line before
-    for (let i = 0; i < splitText.length -3; i++) {
-        if (splitText[i].indexOf('> wrote:') > -1 &&
-            splitText[i + 3].indexOf('>') == 0 ||
-            splitText[i + 2].indexOf('>') == 0 &&
-            splitText[i + 4].indexOf('>') == 0 &&
-            splitText[i + 5].indexOf('Subject: ') == 0) {
-            splitText = splitText.slice(0, i)
-            cleanBody = splitText.join('\n')
-            break
-        }
-    }
-    //Remove forwarded section of emails 
+function removeForwardThread(body) {
+    splitText = body.split('\n')
     splitArray = splitText.indexOf('---------- Forwarded message ---------')
     if (splitArray != -1) {
         splitText = splitText.slice(0, splitArray)
-        cleanBody = splitText.join('\n')
+        body = splitText.join('\n')
     }
-    else { cleanBody = splitText.join('\n') }
+    return body
+}
+
+function removeDisclaimer(body) {
+    //check a parsed email to see if it has a legal disclaimer at the bottom, if so, remove it 
+}
+
+async function parseEmail(body, header) {
+    plainBody = await convertHtml(body)
+    cleanBody = await cleanString(plainBody)
+    cleanBody = await removeExtraData(cleanBody)
+    cleanBody = await removeReplyThread(cleanBody)
+    cleanBody = await removeForwardThread(cleanBody)
+    
     console.log(cleanBody)
     return cleanBody
 }
